957e9b091de4e62931b425ed910c635037274d41
Natan Roberto Xavier <121309547+natanrobertoxavier@users.noreply.github.com>
2024-12-01 11:39:32 -0300

    feat: added reference TokenManagement nuget packege

---
+++ User.Persistence/User.Persistence.Api/Initializer.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.CompilerServices;
+using TokenService.Manager.Controller;
+
+namespace User.Persistence.Api;
+
+public static class Initializer
+{
+    public static void AddTokenManager(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        AddJWTToken(services, configuration);
+    }
+
+    private static void AddJWTToken(IServiceCollection services, IConfiguration configuration)
+    {
+        var sectionLifeTime = configuration.GetRequiredSection("Settings:Jwt:LifeTimeTokenMinutes");
+        var sectionKey = configuration.GetRequiredSection("Settings:Jwt:KeyToken");
+        services.AddScoped(option => new TokenController(int.Parse(sectionLifeTime.Value), sectionKey.Value));
+    }
+}
